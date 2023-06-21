d6bfdc8a477bd608060b731b75695a1d9f6e6f5e
Damon Pitkethley <damonpitkethley53@gmail.com>
2023-06-22 11:36:17 +1200

    fix deployment issues

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,6 @@ import "./globals.css";
 import { AnimatePresence } from "framer-motion";
 import Sidebar from "@/components/sidebar";
 
-export const metadata = {
-  title: "Damon Pitkethley",
-  description: "Portfolio Website",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
