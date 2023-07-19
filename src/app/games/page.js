671889783862138acd06617d30fba119be29d187
Damon Pitkethley <damonpitkethley53@gmail.com>
2023-07-19 22:27:04 +1200

    add game to games section

--- src/app/games/page.js
+++ src/app/games/page.js
@@ -2,8 +2,17 @@ import Wrapper from "@/components/wrapper";
 
 export default function Page() {
   return (
-    <Wrapper>
-      <h1 className="pt-4 text-center text-2xl font-bold font-serif">Under Construction</h1>
+    <Wrapper className="w-full flex justify-center">
+      <iframe
+        frameborder="0"
+        src="https://itch.io/embed/2177443"
+        width="552"
+        height="167"
+      >
+        <a href="https://geekstories.itch.io/web-tycoon">
+          Web Tycoon by GeekStories
+        </a>
+      </iframe>
     </Wrapper>
   );
 }
