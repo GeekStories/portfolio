fae19df88146e58a01d720f477dd20d09b806252
Damon Pitkethley <damonpitkethley53@gmail.com>
2024-06-23 14:23:57 +1200

    update projects page

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -6,51 +6,36 @@ export default function Page() {
       <div className="projectSectionBody">
         <h1 className="sectionTitle">Games</h1>
         <div className="projectsBoxWrapper">
-          <div className="gameProject">
-            <h1 className="gameTitle">Idle Web Tycoon</h1>
-            <a
-              className="projectLink"
-              href="https://geekstories.itch.io/web-tycoon"
-            >
-              Check it out on Itch.io
+          <iframe
+            frameborder="0"
+            src="https://itch.io/embed/2177443?dark=true"
+            width="552"
+            height="167"
+          >
+            <a href="https://geekstories.itch.io/web-tycoon">
+              Web Tycoon by GeekStories
             </a>
-            <p className="gameDescription">
-              Become a web monopoly. Create a website, attract users and sell
-              many ads
-            </p>
-          </div>
-          <div className="gameProject">
-            <h1 className="gameTitle" p>
-              Idle Realtor
-            </h1>
-            <a className="projectLink" href="#">
-              Not hosted at the moment :(
+          </iframe>
+          <iframe
+            frameborder="0"
+            src="https://itch.io/embed/2634398?dark=true"
+            width="552"
+            height="167"
+          >
+            <a href="https://geekstories.itch.io/typewriter">
+              Typewriter by GeekStories
             </a>
-            <p className="gameDescription">
-              Take your shot at becoming a very wealthy realestate agent. Buy
-              property, lease it out, renovate and sell.
-            </p>
-          </div>
-          <div className="gameProject">
-            <h1 className="gameTitle">The Yard</h1>
-            <a className="projectLink" href="#">
-              Not hosted at the moment :(
+          </iframe>
+          <iframe
+            frameborder="0"
+            src="https://itch.io/embed/2772991?linkback=true&amp;bg_color=2f2f2f&amp;fg_color=ffffff&amp;link_color=000000&amp;border_color=585858"
+            width="552"
+            height="167"
+          >
+            <a href="https://geekstories.itch.io/terminal-controller">
+              Terminal Controller by GeekStories
             </a>
-            <p className="gameDescription">
-              You own a car yard. You take in junk cars, fix &apos;em up, move &apos;em on
-              and make a quick buck.
-            </p>
-          </div>
-          <div className="gameProject">
-            <h1 className="gameTitle">Typewriter</h1>
-            <a className="projectLink" href="#">
-              Not hosted at the moment :(
-            </a>
-            <p className="gameDescription">
-              Ever wanted to sell a word? You can here! Buy the alphabet and
-              make a profit.
-            </p>
-          </div>
+          </iframe>
         </div>
       </div>
 
@@ -59,26 +44,19 @@ export default function Page() {
         <div className="projectsBoxWrapper">
           <div className="text-neutral-900 bg-fit bg-center p-1">
             <div className="w-full h-full flex flex-col gap-4 p-2 sm:w-11/12 md:w-8/12 lg:w-1/2 mx-auto">
-              <h1 className="font-bold text-xl">YGO Collector</h1>
+              <h1 className="font-bold text-xl">Paper Scissors Rock</h1>
               <p className="text-neutral-900 font-mono rounded border-[1px] border-neutral-900 bg-neutral-50 bg-opacity-30 backdrop-blur-sm p-1 shadow-lg">
-                This is a tool I originally built for myself to manage my
-                collection of Yugioh trading cards. With this website, users can
-                search from a collection of 30k+ Yu-Gi-Oh! cards. <br /> Card
-                data is sourced from{" "}
-                <a
-                  className="externalLink"
-                  href="https://ygoprodeck.com"
-                  target="_blank"
-                >
-                  ygoprodeck
-                </a>{" "}
-                and stored in my database for use here. Card images are
-                unavaliable because I don&apos;t have anywhere to store and host
-                10k+ images currently. So a placeholder is used in the meantime.
+                This is a simple Paper Scissors Rock game that you can play
+                against the computer. This was the final project as part of my
+                L6 diploma in 2022. We had to integrate a provided model into a React
+                web app, and usign the web cam detect a users hand pose.
+
+                The model is a little funky, try to keep just your hand in the frame, but don't worry too much. As long as it's a clear shot.
+                Check the poses by turning off the camera option in the top right (if not already disabled). The poses in the images are what the model looks for.
               </p>
               <a
                 className="projectLink"
-                href="https://ygocollector.com"
+                href="https://psr.damonpitkethley.com/"
                 target="_blank"
               >
                 Visit Website <BiSolidArrowToRight size="1rem" />
