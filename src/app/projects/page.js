c92d7f3cc31df9dd03d0b3597caa05e0af830243
Damon Pitkethley <damonpitkethley53@gmail.com>
2024-06-26 04:29:03 +1200

    add ygocollector to websites

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -3,47 +3,45 @@ import { BiSolidArrowToRight } from "react-icons/bi";
 export default function Page() {
   return (
     <div className="w-11/12 md:w-9/12 flex flex-col mx-auto gap-5 mt-16">
-      <div className="projectSectionBody">
-        <h1 className="sectionTitle">Games</h1>
-        <div className="projectsBoxWrapper">
-          <iframe
-            frameborder="0"
-            src="https://itch.io/embed/2177443?dark=true"
-            width="552"
-            height="167"
-          >
-            <a href="https://geekstories.itch.io/web-tycoon">
-              Web Tycoon by GeekStories
-            </a>
-          </iframe>
-          <iframe
-            frameborder="0"
-            src="https://itch.io/embed/2634398?dark=true"
-            width="552"
-            height="167"
-          >
-            <a href="https://geekstories.itch.io/typewriter">
-              Typewriter by GeekStories
-            </a>
-          </iframe>
-          <iframe
-            frameborder="0"
-            src="https://itch.io/embed/2772991?linkback=true&amp;bg_color=2f2f2f&amp;fg_color=ffffff&amp;link_color=000000&amp;border_color=585858"
-            width="552"
-            height="167"
-          >
-            <a href="https://geekstories.itch.io/terminal-controller">
-              Terminal Controller by GeekStories
-            </a>
-          </iframe>
-        </div>
+      <h1 className="sectionTitle">Games</h1>
+      <div className="projectsBoxWrapper">
+        <iframe
+          frameborder="0"
+          src="https://itch.io/embed/2177443?dark=true"
+          width="552"
+          height="167"
+        >
+          <a href="https://geekstories.itch.io/web-tycoon">
+            Web Tycoon by GeekStories
+          </a>
+        </iframe>
+        <iframe
+          frameborder="0"
+          src="https://itch.io/embed/2634398?dark=true"
+          width="552"
+          height="167"
+        >
+          <a href="https://geekstories.itch.io/typewriter">
+            Typewriter by GeekStories
+          </a>
+        </iframe>
+        <iframe
+          frameborder="0"
+          src="https://itch.io/embed/2772991?linkback=true&amp;bg_color=2f2f2f&amp;fg_color=ffffff&amp;link_color=000000&amp;border_color=585858"
+          width="552"
+          height="167"
+        >
+          <a href="https://geekstories.itch.io/terminal-controller">
+            Terminal Controller by GeekStories
+          </a>
+        </iframe>
       </div>
 
       <div className="projectSectionBody">
         <h1 className="sectionTitle">Websites</h1>
-        <div className="projectsBoxWrapper">
+        <div className="flex flex-col lg:flex-row gap-5 w-full justify-center border-t-2 border-black p-2">
           <div className="text-neutral-900 bg-fit bg-center p-1">
-            <div className="w-full h-full flex flex-col gap-4 p-2 sm:w-11/12 md:w-8/12 lg:w-1/2 mx-auto">
+            <div className="w-full h-full flex flex-col gap-4 p-2  mx-auto">
               <h1 className="font-bold text-xl">Paper Scissors Rock</h1>
               <p className="text-neutral-900 font-mono rounded border-[1px] border-neutral-900 bg-neutral-50 bg-opacity-30 backdrop-blur-sm p-1 shadow-lg">
                 This is a simple Paper Scissors Rock game that you can play
@@ -59,6 +57,29 @@ export default function Page() {
               <a
                 className="projectLink"
                 href="https://psr.damonpitkethley.com/"
+                target="_blank"
+              >
+                Visit Website <BiSolidArrowToRight size="1rem" />
+              </a>
+            </div>
+          </div>
+
+          <div className="text-neutral-900 bg-fit bg-center p-1">
+            <div className="w-full h-full flex flex-col gap-4 p-2 mx-auto">
+              <h1 className="font-bold text-xl">YGOCollector</h1>
+              <p className="text-neutral-900 font-mono rounded border-[1px] border-neutral-900 bg-neutral-50 bg-opacity-30 backdrop-blur-sm p-1 shadow-lg">
+                This is a tool I built so I could practice using Typescript in
+                NextJS. I use both ssr and csr to render the site; an ExpressJS
+                server on Render that connects to a MongoDB Atlast Cluster for
+                card data, as well as a static site on Netlify serving the
+                11,000+ card images. This was a mighty task for me, as I wasn't
+                sure how to host such a large quantity of images. I have an
+                ExpressJS api hosted on Render, and a server on Netlify acting
+                as a CDN to serve the 11,000+ images.
+              </p>
+              <a
+                className="projectLink"
+                href="https://ygo.damonpitkethley.com/"
                 target="_blank"
               >
                 Visit Website <BiSolidArrowToRight size="1rem" />
