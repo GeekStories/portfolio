2ea27b47553cf633f20632b0ebd8c33f0da552f6
Damon Pitkethley <damonpitkethley53@gmail.com>
2024-08-14 00:03:46 +1200

    remove vercel analytics, add umami

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,20 @@
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 import Header from "@/components/header";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          defer
+          src="https://analytics.board.ddns.net/script.js"
+          data-website-id="391d14f2-956d-489c-a5c1-452c3ed09f41"
+        ></Script>
+      </head>
       <body>
         <Header />
         {children}
-        <Analytics />
       </body>
     </html>
   );
