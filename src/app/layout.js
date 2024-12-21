a3e1b146fd9a234d871ca640e5bbdac14dc84de1
Damon Pitkethley <damonpitkethley53@gmail.com>
2024-12-21 16:41:26 +1300

    remove analytics tracking

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,9 @@
 import "./globals.css";
-import Header from "@/components/header";
 import NavBar from "@/components/NavBar";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Header />
       <body>
         <NavBar />
         {children}
